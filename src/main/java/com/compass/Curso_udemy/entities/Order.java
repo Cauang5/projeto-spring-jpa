969d5dddb507227fdb1a5305b263fb121d6b4625
Cauan Leite <cauanfc@hotmail.com>
2024-06-20 16:55:44 -0300

    OrderStatus Enum

--- src/main/java/com/compass/Curso_udemy/entities/Order.java
+++ src/main/java/com/compass/Curso_udemy/entities/Order.java
@@ -1,5 +1,6 @@
 package com.compass.Curso_udemy.entities;
 
+import com.compass.Curso_udemy.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -14,6 +15,7 @@ import java.time.Instant;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Order implements Serializable {
 
     @Id
@@ -24,7 +26,28 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    @Setter(AccessLevel.NONE)
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.Id = id;
+        this.moment = moment;
+        this.setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
 }
+
