4ab03e949a19d348b79dfcfd7de8c75f5aa41342
Cauan Leite <cauanfc@hotmail.com>
2024-06-23 11:42:14 -0300

    Exception Handling Update

--- src/main/java/com/compass/Curso_udemy/service/UserService.java
+++ src/main/java/com/compass/Curso_udemy/service/UserService.java
@@ -3,6 +3,7 @@ package com.compass.Curso_udemy.service;
 import com.compass.Curso_udemy.entities.User;
 import com.compass.Curso_udemy.repositories.UserRepository;
 import com.compass.Curso_udemy.resources.exception.DatabaseException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -41,9 +42,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
